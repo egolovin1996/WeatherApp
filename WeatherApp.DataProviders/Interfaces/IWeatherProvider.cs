6774f8ae1c67e11b0f1aaa9e412dd2c5d5fd1086
egolovin1996 <egolovin1996@gmail.com>
2019-09-02 17:14:24 +0500

    #9 Add summary

--- WeatherApp.DataProviders/Interfaces/IWeatherProvider.cs
+++ WeatherApp.DataProviders/Interfaces/IWeatherProvider.cs
@@ -2,10 +2,26 @@ using System.Threading.Tasks;
 
 namespace WeatherApp.DataAccess.Interfaces
 {
+    /// <summary>
+    /// Interface for receiving weather data
+    /// </summary>
     public interface IWeatherProvider
     {
+        /// <summary>
+        /// Get weather by city name for a days count
+        /// </summary>
+        /// <param name="cityName">City name</param>
+        /// <param name="daysCount">The number of days to get the weather</param>
+        /// <returns>Weather in json format</returns>
         Task<string> GetWeatherByCityName(string cityName, int daysCount);
 
+        /// <summary>
+        /// Get weather by coordinates for a days count
+        /// </summary>
+        /// <param name="latitude">Latitude</param>
+        /// <param name="longitude">Longitude</param>
+        /// <param name="daysCount">The number of days for getting the weather</param>
+        /// <returns>Weather in json format</returns>
         Task<string> GetWeatherByCoords(double latitude, double longitude, int daysCount);
     }
 }
--- WeatherApp.Web/Controllers/WeatherController.cs
+++ WeatherApp.Web/Controllers/WeatherController.cs
@@ -4,6 +4,9 @@ using WeatherApp.DataAccess.Interfaces;
 
 namespace WeatherApp.Web.Controllers
 {
+    /// <summary>
+    /// Controller for receiving weather data
+    /// </summary>
     [Route("api/weather")]
     public class WeatherController : Controller
     {
@@ -14,12 +17,25 @@ namespace WeatherApp.Web.Controllers
             _weatherProvider = weatherProvider;
         }
 
+        /// <summary>
+        /// Get weather by city name for a days count
+        /// </summary>
+        /// <param name="cityName">City name</param>
+        /// <param name="daysCount">The number of days to get the weather</param>
+        /// <returns>Weather in json format</returns>
         [HttpGet("getWeatherByCityName/{cityName}/{daysCount}")]
         public async Task<string> GetWeatherByCityName(string cityName, int daysCount)
         {
             return await _weatherProvider.GetWeatherByCityName(cityName, daysCount);
         }
-        
+
+        /// <summary>
+        /// Get weather by coordinates for a days count
+        /// </summary>
+        /// <param name="latitude">Latitude</param>
+        /// <param name="longitude">Longitude</param>
+        /// <param name="daysCount">The number of days for getting the weather</param>
+        /// <returns>Weather in json format</returns>
         [HttpGet("getWeatherByCoords/{latitude}/{longitude}/{daysCount}")]
         public async Task<string> GetWeatherByCoords(double latitude, double longitude, int daysCount)
         {
